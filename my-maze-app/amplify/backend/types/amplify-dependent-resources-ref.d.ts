3da74cf8e9a98cc6914dc22bce1e77c53a42dfa1
j.oduyemi <j.oduyemi@yahoo.com>
2023-11-11 17:05:18 +0000

    starting the hosting process

---
+++ my-maze-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myflaskapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "myflaskfunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
